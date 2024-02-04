3b52a5b55d5efa29e8f9f884d7c96d8f9d085e20
Mariam <mariam.mantidze1@gmail.com>
2024-02-04 19:10:45 +0400

    add toggle to questions

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
 /* .arrow:hover {
   transform: rotate(180deg);
 } */
+const question = document.getElementsByClassName("faq-question");
+
+Array.from(question).forEach((question) =>
+  question.addEventListener("click", () => {
+    const commonParent = question.closest(".faq-item");
+    const currentArrow = commonParent.querySelector(".arrow");
+    const answer = question.nextElementSibling;
+
+    answer.classList.toggle("faq-answer-hidden");
+
+    if (answer.classList.contains("faq-answer-hidden")) {
+      answer.style.display = "block";
+      currentArrow.style.transform = "rotate(180deg)";
+    } else {
+      answer.style.display = "";
+      currentArrow.style.transform = "";
+    }
+  })
+);
