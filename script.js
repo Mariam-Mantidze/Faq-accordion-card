4380ea1630355e36fbff43e606abd4b3c7aa0d55
Mariam <mariam.mantidze1@gmail.com>
2024-02-07 13:46:45 +0400

    refactor accordion functional

--- script.js
+++ script.js
@@ -1,15 +1,39 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const question = document.querySelectorAll(".clickable");
+// document.addEventListener("DOMContentLoaded", function () {
+//   const element = document.querySelectorAll(".clicked");
 
-  question.forEach((question) =>
-    question.addEventListener("click", () => {
-      const commonParent = question.closest(".faq-item");
-      const currentArrow = commonParent.querySelector(".arrow");
+//   element.forEach((element) =>
+//     element.addEventListener("click", (event) => {
+//       const commonParent = element.closest(".faq-item");
+//       const currentArrow = commonParent.querySelector(".arrow");
+//       const question = commonParent.querySelector(".faq-question");
+//       let answer;
+
+//       if (element.nextElementSibling.classList.contains("faq-answer")) {
+//         answer = element.nextElementSibling;
+//       } else {
+//         answer = element.previousElementSibling;
+//       }
+
+//       answer.classList.toggle("answer-active");
+//       currentArrow.classList.toggle("arrow-active");
+//       question.classList.toggle("question-active");
+//     })
+//   );
+// });
+
+document.addEventListener("DOMContentLoaded", function () {
+  const element = Array.from(document.getElementsByClassName("faq-item"));
+  console.log(element);
+
+  element.forEach((element) => {
+    element.addEventListener("click", (event) => {
+      const question = element.querySelector(".faq-question");
+      const arrow = element.querySelector(".arrow");
       const answer = question.nextElementSibling;
 
       answer.classList.toggle("answer-active");
-      currentArrow.classList.toggle("arrow-active");
+      arrow.classList.toggle("arrow-active");
       question.classList.toggle("question-active");
-    })
-  );
+    });
+  });
 });
