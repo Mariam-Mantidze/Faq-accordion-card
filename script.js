6c6b6389e8a40defdc74c8994d69aa5ebf37d89d
Mariam <mariam.mantidze1@gmail.com>
2024-02-07 16:12:53 +0400

    try new logic of function

--- script.js
+++ script.js
@@ -16,6 +16,43 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 });
 
-// const question = element.querySelector(".faq-question");
-// const arrow = element.querySelector(".arrow");
-// const answer = question.nextElementSibling;
+// ================ [BETTER VERSION WITH COMMENTS] =================
+
+// document.addEventListener("DOMContentLoaded", function () {
+//   // variable for keeping last active element
+//   let lastActive = null;
+
+//   const elements = Array.from(document.getElementsByClassName("faq-item"));
+
+//   elements.forEach((element) => {
+//     element.addEventListener("click", (event) => {
+//       // variable for keeping current active element
+//       const currentParent = event.target.parentElement;
+//       // ------- test
+//       console.log(currentParent, "current");
+
+//       // check if current parent does not equal lastActive parent
+//       if (currentParent != lastActive) {
+//         // if lastActive exists remove active class from it
+//         if (lastActive) {
+//           lastActive.classList.remove("active");
+//         }
+//         // add active class to current element
+//         currentParent.classList.add("active");
+//         // assign current element's referrence to lastActive element
+//         lastActive = currentParent;
+//         // -------- test
+//         console.log(lastActive, "lastActive");
+//         // and if last active and current element are equal
+//       } else {
+//         // toggle the active class which causes the "unclicking behaviour"
+//         currentParent.classList.toggle("active");
+//         // reset active element. so the that the next click is treated as an activation
+//         // regardless of the element's prev. state.
+//         if (!currentParent.classList.contains("active")) {
+//           lastActive = null;
+//         }
+//       }
+//     });
+//   });
+// });
