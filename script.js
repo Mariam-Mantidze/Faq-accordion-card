34900e4bfa9d4df2a2bf4b25da08f1726ea15fa6
Mariam <mariam.mantidze1@gmail.com>
2024-02-07 15:44:58 +0400

    refactor functional

--- script.js
+++ script.js
@@ -1,39 +1,21 @@
-// document.addEventListener("DOMContentLoaded", function () {
-//   const element = document.querySelectorAll(".clicked");
-
-//   element.forEach((element) =>
-//     element.addEventListener("click", (event) => {
-//       const commonParent = element.closest(".faq-item");
-//       const currentArrow = commonParent.querySelector(".arrow");
-//       const question = commonParent.querySelector(".faq-question");
-//       let answer;
-
-//       if (element.nextElementSibling.classList.contains("faq-answer")) {
-//         answer = element.nextElementSibling;
-//       } else {
-//         answer = element.previousElementSibling;
-//       }
-
-//       answer.classList.toggle("answer-active");
-//       currentArrow.classList.toggle("arrow-active");
-//       question.classList.toggle("question-active");
-//     })
-//   );
-// });
-
 document.addEventListener("DOMContentLoaded", function () {
-  const element = Array.from(document.getElementsByClassName("faq-item"));
-  console.log(element);
+  const elements = Array.from(document.getElementsByClassName("faq-item"));
 
-  element.forEach((element) => {
+  elements.forEach((element) => {
     element.addEventListener("click", (event) => {
-      const question = element.querySelector(".faq-question");
-      const arrow = element.querySelector(".arrow");
-      const answer = question.nextElementSibling;
+      const isActive = event.target.parentElement.classList.contains("active");
 
-      answer.classList.toggle("answer-active");
-      arrow.classList.toggle("arrow-active");
-      question.classList.toggle("question-active");
+      elements.forEach((element) => {
+        element.classList.remove("active");
+      });
+
+      if (!isActive) {
+        event.target.parentElement.classList.add("active");
+      }
     });
   });
 });
+
+// const question = element.querySelector(".faq-question");
+// const arrow = element.querySelector(".arrow");
+// const answer = question.nextElementSibling;
