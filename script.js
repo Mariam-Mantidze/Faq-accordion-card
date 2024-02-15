1f3c8327b890d417a6383ca425c3f540432b6168
Mariam <mariam.mantidze1@gmail.com>
2024-02-15 12:36:40 +0400

    refactor functional

--- script.js
+++ script.js
@@ -3,14 +3,15 @@ document.addEventListener("DOMContentLoaded", function () {
 
   elements.forEach((element) => {
     element.addEventListener("click", (event) => {
-      const isActive = event.target.parentElement.classList.contains("active");
+      const isActive = event.currentTarget.classList.contains("active");
 
+      console.log(event.currentTarget, event.target.parentElement);
       elements.forEach((element) => {
         element.classList.remove("active");
       });
 
       if (!isActive) {
-        event.target.parentElement.classList.add("active");
+        event.currentTarget.classList.add("active");
       }
     });
   });
@@ -18,16 +19,16 @@ document.addEventListener("DOMContentLoaded", function () {
 
 // ================ [BETTER VERSION WITH COMMENTS] =================
 
+// const elements = Array.from(document.getElementsByClassName("faq-item"));
+
 // document.addEventListener("DOMContentLoaded", function () {
 //   // variable for keeping last active element
 //   let lastActive = null;
 
-//   const elements = Array.from(document.getElementsByClassName("faq-item"));
-
 //   elements.forEach((element) => {
 //     element.addEventListener("click", (event) => {
 //       // variable for keeping current active element
-//       const currentParent = event.target.parentElement;
+//       const currentParent = event.currentTarget;
 //       // ------- test
 //       console.log(currentParent, "current");
 
@@ -36,13 +37,14 @@ document.addEventListener("DOMContentLoaded", function () {
 //         // if lastActive exists remove active class from it
 //         if (lastActive) {
 //           lastActive.classList.remove("active");
+//           console.log(lastActive, "last active");
 //         }
 //         // add active class to current element
 //         currentParent.classList.add("active");
 //         // assign current element's referrence to lastActive element
 //         lastActive = currentParent;
 //         // -------- test
-//         console.log(lastActive, "lastActive");
+//         // console.log(lastActive, "lastActive");
 //         // and if last active and current element are equal
 //       } else {
 //         // toggle the active class which causes the "unclicking behaviour"
